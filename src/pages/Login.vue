fdafb9b97fdaefc40d59ca3928321e9b34fc71be
hbb <375152092@qq.com>
2019-04-25 21:20:28 +0800

    7表单的验证完成

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="login-form">
-    <el-form :model="form" label-width="100px">
+    <el-form :model="form" label-width="100px" :rules="rules">
       <p class="login-title">登录</p>
-      <el-form-item label="账号" prop="pass">
+
+      <!-- prop 指定规则查找 -->
+      <el-form-item label="账号" prop="uname">
         <!-- 双向数据绑定 -->
+
         <el-input type="text" v-model="form.uname"></el-input>
       </el-form-item>
-      <el-form-item label="密码" prop="checkPass">
+      <el-form-item label="密码" prop="upwd">
         <el-input type="password" v-model="form.upwd"></el-input>
       </el-form-item>
       <el-form-item class="fromSubmit">
@@ -24,6 +27,25 @@ export default {
       form: {
         uname: "",
         upwd: ""
+      },
+      //这里进行表单的验证
+      rules: {
+        //多条规则，不同状态下面触发
+        //require 是否必填
+        //message是报错信息
+        //trigger是什么时候触发
+        uname: [
+          {
+            required: true,
+            message: "请输入账号",
+            trigger: "blur"
+          }
+        ],
+        upwd: [{ 
+          required: true, 
+          message: "请输入密码", 
+          trigger: "blur" 
+          }]
       }
     };
   },
@@ -32,27 +54,27 @@ export default {
       console.log("提交");
       this.$axios
         .post("http://127.0.0.1:8899/admin/account/login", this.form)
-        .then((res)=> {
+        .then(res => {
           console.log(res);
           // 使用解构赋值的方式
-          const {status,message}=res.data;
-            if(status==0){
-              //登录成功，跳转页面,注意这里的this.$router假如使用function的话，是不能识别router，因为this 的指向不一样，要使用箭头函数，this 的指向就是window，function的this指向是上面的对象
-              this.$router.push('./admin');
-            }else{
-              //登录失败
-              this.$message({
-                message:message,
-                type:'success'
-              })
-              // alert('登陆失败')
-            }
+          const { status, message } = res.data;
+          if (status == 0) {
+            //登录成功，跳转页面,注意这里的this.$router假如使用function的话，是不能识别router，因为this 的指向不一样，要使用箭头函数，this 的指向就是window，function的this指向是上面的对象
+            this.$router.push("./admin");
+          } else {
+            //登录失败
+            this.$message({
+              message: message,
+              type: "success"
+            });
+            // alert('登陆失败')
+          }
         });
     },
-    oncancle:function(){
+    oncancle: function() {
       //点击取消清除掉表单元素
-      this.form.uname="";
-      this.form.upwd="";
+      this.form.uname = "";
+      this.form.upwd = "";
     }
   }
 };
@@ -64,7 +86,6 @@ export default {
   left: 50%;
   top: 50%;
   margin: -200px 0 0 -225px;
-  
 }
 .login-title {
   text-align: center;
